53630d42fd8253f3ea5cb52737b8c5fb99945611
Wes Reed <wes@wesreed.co>
2018-06-29 11:57:44 -0600

    adds scripts and styleshees

--- app/public/wp-content/plugins/plugin-dev-101/index.php
+++ app/public/wp-content/plugins/plugin-dev-101/index.php
@@ -99,3 +99,11 @@ function pd101_message_shortcode( $atts ) {
 	return '<div class="message ' . esc_attr( $atts['color'] ) . '">' . esc_html( $atts['text'] ) . '</div>';
 }
 add_shortcode( 'message', 'pd101_message_shortcode' );
+
+
+
+function pd101_load_styles() {
+	wp_enqueue_style( 'pd101-styles', plugins_url( 'pd101-styles.css', __FILE__) );
+	wp_enqueue_script( 'pd101-scripts', plugins_url( 'pd101-scripts.js', __FILE__), array('jquery'), '1.0');
+}
+add_action( 'wp_enqueue_scripts', 'pd101_load_styles' );
